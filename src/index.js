851c7bd3c68e8daeb6a92fd6d3e96a6e945a6766
mattpe <mattpe@metropolia.fi>
2021-01-14 15:14:58 +0200

    Week 1 Task 3 almost ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,95 @@
-console.log('Hello console!');
+const coursesEn = ["Hamburger, cream sauce and poiled potates",
+  "Goan style fish curry and whole grain rice",
+  "Vegan Chili sin carne and whole grain rice",
+  "Broccoli puree soup, side salad with two napas",
+  "Lunch baguette with BBQ-turkey filling",
+  "Cheese / Chicken / Vege / Halloum burger and french fries"];
+const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+  "Goalaista kalacurrya ja täysjyväriisiä",
+  "vegaani Chili sin carne ja täysjyväriisi",
+  "Parsakeittoa,lisäkesalaatti kahdella napaksella",
+  "Lunch baguette with BBQ-turkey filling",
+  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+
+let languageSetting = 'fi';
+
+
+/**
+ * Displays lunch menu items as html list
+ *
+ * @param {Array} menu - Lunch menu array
+ */
+const renderMenu = (menu) => {
+  const list = document.querySelector('#sodexo');
+  list.innerHTML = '';
+  for (const item of menu) {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    list.appendChild(listItem);
+  }
+};
+
+
+/**
+ * Switch app lang en/fi
+ */
+const switchLanguage = () => {
+  if (languageSetting === 'fi') {
+    languageSetting = 'en';
+    renderMenu(coursesEn);
+  } else {
+    languageSetting = 'fi';
+    renderMenu(coursesFi);
+  }
+  console.log('change language to: ', languageSetting);
+};
+
+/**
+ * Sorts menu alphapetically
+ *
+ * @param {Array} menu
+ * @param {string} order
+ * @returns Sorted menu array
+ */
+const sortMenu = (menu, order) => {
+  if (order == 'desc') {
+    return menu.sort().reverse();
+  } else {
+    return menu.sort();
+  }
+};
+
+/**
+ * Eventhandler for sort menu button
+ */
+const renderSortedMenu = () => {
+  if (languageSetting === 'en') {
+    renderMenu(sortMenu(coursesEn, 'asc'));
+  } else {
+    renderMenu(sortMenu(coursesFi, 'desc'));
+  }
+};
+
+/**
+ * Picks a random dish from lunch menu array
+ *
+ * @param {Array} menu
+ * @returns string dish name
+ */
+const pickRandomDish = (menu) => {
+  const randomIndex = Math.floor(Math.random() * menu.length);
+  return menu[randomIndex];
+};
+
+const displayRandomDish = () => {
+  alert(pickRandomDish(coursesFi));
+};
+
+
+const init = () => {
+  document.querySelector('#switch-lang').addEventListener('click', switchLanguage);
+  document.querySelector('#sort-menu').addEventListener('click', renderSortedMenu);
+  document.querySelector('#pick-dish').addEventListener('click', displayRandomDish);
+  renderMenu(coursesFi);
+};
+init();
