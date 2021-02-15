b3e072512448e862d30d74eb0e9153981b868c4a
mattpe <mattpe@metropolia.fi>
2021-02-15 14:41:01 +0200

    add quick'n'dirty hsl data fetch example

--- src/modules/network.js
+++ src/modules/network.js
@@ -27,4 +27,34 @@ const fetchGetJson = async (url, useProxy = false) => {
   return responseJson;
 };
 
-export {fetchGetJson};
+/**
+ * Creates HTTP POST request
+ *
+ * @param {string} url API endpoint
+ * @param {string} contentType
+ * @param {Object} body request payload
+ * @param {boolean} useProxy wheter to use the proxy server
+ * @returns {Object} json data
+ */
+const fetchPostJson = async (url, contentType, body, useProxy = false) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': contentType
+    },
+    body: body,
+  };
+  let response;
+  try {
+    response = await fetch(`${useProxy ? networkProxyUrl : ''}${url}`, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error('network fetchPost error', error.message);
+  }
+  const responseJson = await response.json();
+  return responseJson;
+};
+
+export {fetchGetJson, fetchPostJson};
